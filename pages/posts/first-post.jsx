a584c918c8af02b63634d8a0e715a91446ab17de
elgrenka <bugigen@yandex.ru>
2022-06-18 21:01:00 +0400

    vv

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,17 +1,22 @@
 import Link from "next/link";
-import styles from '../../styles/Home.module.css';
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <div>
-      <h1>First post</h1>
-      <h2 className={styles.title}>
+    <Layout>
+      <Head key={'head'}>
+        <title>First Post</title>
+      </Head>
+
+      <h1>It is first post</h1>
+      <h2>
         <Link href="/">
           <a>Back to Home</a>
         </Link>
       </h2>
       
-    </div>
-  )
+    </Layout>
+  );
 }
 
