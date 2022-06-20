37cc40de9aa7f3b0810bf411bc01311b59b0b759
elgrenka <bugigen@yandex.ru>
2022-06-20 21:13:51 +0400

    styles

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -5,16 +5,12 @@ import Layout from "../../components/layout";
 export default function FirstPost() {
   return (
     <Layout>
-      <Head key={'head'}>
+      {/* <Head key={'head'}> */}
+      <Head>
         <title>First Post</title>
       </Head>
 
       <h1>It is first post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to Home</a>
-        </Link>
-      </h2>
       
     </Layout>
   );
